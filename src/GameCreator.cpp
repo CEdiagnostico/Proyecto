2cb932c400b8f846ab73e4603f3027e15c47eb92
roberto <rbj1504@hotmail.com>
2015-04-25 00:35:04 -0600

    json plus socket

--- src/GameCreator.cpp
+++ src/GameCreator.cpp
@@ -7,11 +7,7 @@
 void* GameCreator::create(){
     void* ret = malloc(sizeof(Member) * 7);
     *static_cast<Jugador*>(ret) = Jugador(1,0,0);
-    for(int i=1; i<4; i++){
-        *static_cast<Obstaculo*>(ret+i*sizeof(Member)) = Obstaculo(i+1,0,0);
-    }
-    for(int i=4; i<7; i++){
-        *static_cast<Enemigo*>(ret+i*sizeof(Member)) = Enemigo(i+1,0,0);
-    }
+    for(int i=1; i<4; i++) *static_cast<Obstaculo*>(ret+i*sizeof(Member)) = Obstaculo(i+1,0,0);
+    for(int i=4; i<7; i++) *static_cast<Enemigo*>(ret+i*sizeof(Member)) = Enemigo(i+1,0,0);
     return ret;
 };
